9011aa101a6b9e7a11a7dcc429e432f32dbf8df3
Matt Pestritto <matt@pestritto.com>
2019-03-11 19:19:03 -0400

    remove outdated library so dockerfile builds

--- Gruntfile.js
+++ Gruntfile.js
@@ -317,11 +317,11 @@ module.exports = function (grunt) {
     },
 
     // Replace Google CDN references
-    cdnify: {
-      dist: {
-        html: ['<%= yeoman.dist %>/*.html']
-      }
-    },
+    // cdnify: {
+    //   dist: {
+    //     html: ['<%= yeoman.dist %>/*.html']
+    //   }
+    // },
 
     // Copies remaining files to places other tasks can use
     copy: {
@@ -422,7 +422,6 @@ module.exports = function (grunt) {
     'concat',
     'ngAnnotate',
     'copy:dist',
-    'cdnify',
     'cssmin',
     'uglify',
     'filerev',
